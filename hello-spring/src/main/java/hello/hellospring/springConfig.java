2eb762561fa06a9aa0fbe745c0ff96b90db38633
rt3310 <btac3310@gmail.com>
2022-01-18 22:33:34 +0900

    update spring

---
+++ hello-spring/src/main/java/hello/hellospring/springConfig.java
@@ -0,0 +1,53 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.*;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class springConfig {
+//    private DataSource dataSource;
+
+//    @Autowired
+//    public springConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+//    private EntityManager em;
+//@Autowired
+//public springConfig(EntityManager em) {
+//    this.em = em;
+//}
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public springConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
+}
